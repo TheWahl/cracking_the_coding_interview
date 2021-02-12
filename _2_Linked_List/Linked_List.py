1959c1a575e75a5bb9c7e073bd7fc2e9f200a2b8
Jake Wahl <jwahl@sandiego.edu>
2021-02-11 21:05:47 -0800

    Finished LinkedList class and started problem 2.1

---
+++ _2_Linked_List/Linked_List.py
@@ -0,0 +1,115 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self, val=None):
+        self.head = None if val == None else Node(val)
+        self._length = 0 if val == None else 1
+
+    def insert(self, val, idx=0):
+
+        n = Node(val)
+        if idx == 0:  # Insert at head of list
+            if self.isEmpty():
+                self.head = n
+            else:
+                n.next = self.head
+                self.head = n
+        else:  # Insert at specified index
+            if idx < 0:
+                raise IndexError
+            if idx > self._length:
+                idx = self._length
+
+            cur = self.head
+            prev = None
+            while idx != 0:
+                prev = cur
+                cur = cur.next
+                idx -= 1
+            n.next = prev.next
+            prev.next = n
+
+        self._length += 1
+
+    def append(self, val):
+
+        n = Node(val)
+        if self.isEmpty():
+            self.head = n
+        else:
+            cur = self.head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = n
+
+        self._length += 1
+
+    def remove(self, idx=0):
+        if self._length == 0:
+            raise IndexError('List is empty!')
+        elif idx >= self._length or idx < 0:
+            raise IndexError('Index out of Bounds!')
+
+        if idx == 0:  # Remove at head of list
+            self.head = self.head.next
+        else:  # Remove at specified index
+            cur = self.head
+            prev = None
+            while idx != 0:
+                prev = cur
+                cur = cur.next
+                idx -= 1
+            prev.next = cur.next
+
+        self._length -= 1
+
+    def get(self, idx):
+        if self._length == 0:
+            raise IndexError('List is empty')
+        elif idx >= self._length or idx < 0:
+            raise IndexError('Index out of Bounds')
+
+        cur = self.head
+        while idx != 0:
+            cur = cur.next
+            idx -= 1
+        return cur.val
+
+    def find(self, val):
+        idx = 0
+        cur = self.head
+        while cur != None:
+            if cur.val == val:
+                return idx
+            else:
+                cur = cur.next
+                idx += 1
+
+        return -1
+
+    def __str__(self):
+        if self.head == None:
+            return 'List Empty'
+        else:
+            cur = self.head
+            string = ''
+            while cur != None:
+                if cur.next != None:
+                    string += f'{cur.val} -> '
+                    cur = cur.next
+                else:  # Last element
+                    string += f'{cur.val}'
+                    cur = cur.next  # breaking loop
+
+            return string
+
+    def __len__(self):
+        return self._length
+
+    def isEmpty(self):
+        return self._length == 0
--- _2_Linked_List/_1_Remove_Dups.py
+++ _2_Linked_List/_1_Remove_Dups.py
@@ -4,6 +4,22 @@
 # Problem 2.1 - Write code to remove duplicates from an unsorted linked list.
 #               FOLLOW UP: How would you solve this problem if a temporary buffer is not allowed?
 
-from common.Linked_List import LinkedList
+from Linked_List import LinkedList
 
-a = LinkedList()
+
+def removeDuplicates(list: LinkedList) -> LinkedList:
+
+    if len(list) <= 1:  # List is comprised of 1 or 0 elements
+        return list
+
+    cur = list.head
+    while cur.next != None:
+
+    return list
+
+
+test_case = LinkedList()
+for i in [2, 2, 2]:
+    test_case.append(i)
+
+print(removeDuplicates(test_case))
