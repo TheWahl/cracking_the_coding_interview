b5ee3b7272a93b03058ebb2fdfe44cb514ed442b
Jake Wahl <jwahl@sandiego.edu>
2021-04-06 19:16:45 -0700

    Started work on Problem 8.3.

---
+++ _8_Recursion/_3_Magic_Index.py
@@ -0,0 +1,11 @@
+# Jacob Wahl
+# 4/6/21
+#
+# Problem 8.3 - A magic index in an array A[0 ... n-1] is defined to be an
+#               index such that A[i] = i. Given a sorted array of distinct,
+#               write a method to find a magic index, if one exists, in array
+#               A.
+#               FOLLOW UP: What if the values are not distinct?
+
+def find_magic_index(A):
+    pass  # Implement binary search
